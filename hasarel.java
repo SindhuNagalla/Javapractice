8b121f1999fa74d7eb3b32668aef932d4e7e19a6
Sindhu <nagalla.sindhu1@gmail.com>
2025-02-03 11:41:16 +0530

    has-a relationship

---
+++ hasarel.java
@@ -0,0 +1,16 @@
+class a
+{
+	void show()
+	{
+		System.out.println("Welcome");
+	}
+}
+class hasarel 
+{
+	a a1=new a();
+	public static void main(String[] args) 
+	{
+		hasarel h=new hasarel();
+		h.a1.show();
+	}
+}
