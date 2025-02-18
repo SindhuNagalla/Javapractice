43fe9474808b4f1bb5d178f09f1ddc853c6b8e88
Sindhu <nagalla.sindhu1@gmail.com>
2025-02-18 22:42:01 +0530

    Strings with equals and ==

---
+++ Strings/String11.java
@@ -0,0 +1,10 @@
+class String11 
+{
+	public static void main(String[] args) 
+	{
+		String s1="Welcome";
+		String s2="Welcome";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1==s2);
+	}
+}
