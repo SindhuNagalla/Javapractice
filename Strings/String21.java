5c3e58dd9d466172dd2798db80b2c80d97f7e744
Sindhu <nagalla.sindhu1@gmail.com>
2025-02-19 16:01:44 +0530

    converting octal,hexadecimal,binary to decimal by using 0 for octal,0x for hexadecimal ,0b for binary

---
+++ Strings/String21.java
@@ -0,0 +1,14 @@
+class String21 
+{
+	public static void main(String[] args) 
+	{
+		int x=30;
+		int y=036;//to convert Octal to decimal use '0'
+		int z=0x1e;//to convert Hexa decimal to decimal use 0x or 0X
+		int a=0b11110;//to convert Binary to decimal use 0b or 0B
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(a);
+	}
+}
