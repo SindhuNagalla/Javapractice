c638f279a33bfda0846052d7d7142d8a5ea1b2e5
Sindhu <nagalla.sindhu1@gmail.com>
2025-02-19 15:19:33 +0530

    Command line arguments

---
+++ Strings/String16.java
@@ -0,0 +1,7 @@
+class  String16
+{
+	public static void main(String[] args) //it is also a string method
+	{
+		System.out.println(args[0]+args[1]);//the values are stored like string
+	}
+}
---
+++ Strings/String17.java
@@ -0,0 +1,10 @@
+class String17 
+{
+	public static void main(String[] args) 
+	{
+		for(String s1:args)
+		{
+			System.out.println(s1);
+		}
+	}
+}
