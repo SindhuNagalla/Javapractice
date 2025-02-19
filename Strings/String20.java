1edaf677dd162326bdce5baefcf44d6c5d4eb8a4
Sindhu <nagalla.sindhu1@gmail.com>
2025-02-19 15:58:46 +0530

    converting decimal to octal,hexadecimal,binary by using methods toOctString(),toHexString(),toBinaryString()

---
+++ Strings/String20.java
@@ -0,0 +1,11 @@
+class  String20
+{
+	public static void main(String[] args) 
+	{
+		int x=30;
+		System.out.println(x);
+		System.out.println(Integer.toOctalString(x));//to convert nuumber from decimal number to octal number by using method toOctString return type is string  parameter is int
+		System.out.println(Integer.toHexString(x));//to convert number from decimal number to Hexadecimal number by using method toHexString return type is string,parameter is int
+		System.out.println(Integer.toBinaryString(x));//to convert number from decimal number to Binary number by using method toBinaryString return type is string,parameter is int
+	}
+}
