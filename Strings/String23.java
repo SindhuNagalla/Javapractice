3743b97a1abef947cee0ebdba9eb66f141e11c8b
Sindhu <nagalla.sindhu1@gmail.com>
2025-02-19 16:15:14 +0530

    here string is drectly converting into integer without using conversion and by using import statement import static java.lang.Integer.*

---
+++ Strings/String23.java
@@ -0,0 +1,12 @@
+import static java.lang.Integer.*;
+import static java.lang.System.*;
+
+class String23 
+{
+	public static void main(String[] args) 
+	{
+		int x=parseInt(args[0]);
+		int y=parseInt(args[1]);
+		out.println(x+y);
+	}
+}
